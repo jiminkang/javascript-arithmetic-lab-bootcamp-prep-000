4005d0893dbdd03960096b5131c153ec4f2fb460
Jimin Kang <jimin.kang91@gmail.com>
2017-12-17 19:51:40 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a,b){
+  
+}
+
+function subract(a,b){
+  
+}
+
+function multiply (a,b){
+}
+
+function divice (a,b){
+
+}
+
+function inc(n){
+
+}
+
+function dec(n){
+}
+
+function makeInt(n){
+}
+
+function preserveDecimal(n){
+}
+}
