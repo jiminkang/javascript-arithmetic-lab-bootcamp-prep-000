bdc195d38cd16620ba799d97d4137c40444005f2
Jimin Kang <jimin.kang91@gmail.com>
2017-12-17 19:52:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ function add(a,b){
   return a+b
 }
 
-function subract(a,b){
+function subtract(a,b){
   return a-b
 }
 
 function multiply (a,b){
+  return a*b
 }
 
 function divice (a,b){
+  return a/b
 
 }
 
